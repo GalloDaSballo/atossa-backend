2cc44b61a5e8e8d02ef7efd73acb449f647905d3
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-22 15:20:20 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '60ee9880fd5727bea7b817b4193fc25f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd95eff42-d249-4b83-9035-251330524446'
+};
